a86f800866d7cf1c8ff11d54b9d4351187bbeb6d
krishnajhapate <ki54.jhapate@gmail.com>
2022-01-22 08:53:47 +0530

    added database and file

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
 import imp
-from typing import List
-from fastapi import FastAPI, WebSocket
+from typing import List, Optional
+from fastapi import FastAPI, WebSocket, Request, Depends, HTTPException
 from fastapi.responses import HTMLResponse
 from databases import Database
 from fastapi_jwt_auth import AuthJWT
 from pydantic import BaseModel
+from fastapi_jwt_auth.exceptions import AuthJWTException
+from fastapi.responses import JSONResponse
+
+# database connection
+app = FastAPI()
+database = Database("sqlite:///database.db")
 
 
 class User(BaseModel):
     username: str
     password: str
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: Optional[str] = None
 
 
 class Settings(BaseModel):
@@ -21,18 +36,45 @@ class Settings(BaseModel):
 def get_config():
     return Settings()
 
+
 # exception handler for authjwt
 # in production, you can tweak performance using orjson response
 @app.exception_handler(AuthJWTException)
 def authjwt_exception_handler(request: Request, exc: AuthJWTException):
-    return JSONResponse(
-        status_code=exc.status_code,
-        content={"detail": exc.message}
-    )
+    return JSONResponse(status_code=exc.status_code,
+                        content={"detail": exc.message})
 
-# database connection
-app = FastAPI()
-database = Database("sqlite:///test.db")
+
+# provide a method to create access tokens. The create_access_token()
+# function is used to actually generate the token to use authorization
+# later in endpoint protected
+@app.post('/login')
+def login(user: User, Authorize: AuthJWT = Depends()):
+    if user.username != "test" or user.password != "test":
+        raise HTTPException(status_code=401, detail="Bad username or password")
+
+    # subject identifier for who this token is for example id or username from database
+    access_token = Authorize.create_access_token(subject=user.username)
+    return {"access_token": access_token}
+
+
+# protect endpoint with function jwt_required(), which requires
+# a valid access token in the request headers to access.
+@app.get('/user')
+def user(Authorize: AuthJWT = Depends()):
+    Authorize.jwt_required()
+
+    current_user = Authorize.get_jwt_subject()
+    return {"user": current_user}
+
+
+@app.get('/partially-protected')
+def partially_protected(Authorize: AuthJWT = Depends()):
+    Authorize.jwt_optional()
+
+    # If no jwt is sent in the request, get_jwt_subject() will return None
+    current_user = Authorize.get_jwt_subject() or "anonymous"
+    return {"user": current_user}
 
 
 @app.on_event("startup")
